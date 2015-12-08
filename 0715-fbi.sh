7e71df42fcc1c6f012a775ed2c78504dde47091c
Paul Elder <paul374@localhost.localdomain>
2015-12-08 12:06:04 +0900

    Changed a potential cd issue in the 0715-fbi.sh script

--- 0715-fbi.sh
+++ 0715-fbi.sh
@@ -6,5 +6,4 @@ today=$(date +%Y%m%d)
 # Date of cvf
 d=$(head -n 1 /home/pi/noticeboard/cvf)
 
-cd /home/pi/noticeboard/$d/
-fbi -m 1920x1080 -a -t 3 --noverbose ./*
+fbi -m 1920x1080 -a -t 3 --noverbose /home/noticeboard/$d/*
